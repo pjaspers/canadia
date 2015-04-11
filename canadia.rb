c5c4df3e88275ca502fbd4480411482c8c5a3730
pjaspers <piet@pjaspers.com>
2015-04-11 20:00:19 +0200

    Supporting images and videos.

--- canadia.rb
+++ canadia.rb
@@ -3,33 +3,36 @@ require "sinatra"
 require "haml"
 
 def random_sentence
-  ["Chuck Norris once visited and described it as lukewarm.",
-   "Sean Bean will never visit it.",
-   "pinguins moved away.",
-   "every drink is on the rocks.",
-   "they call icehockey, hockey.",
-   "they don't call it skiing, they call it walking.",
-   "they call icefishing, fishing.",
-   "every bear in canada is an icebear.",
-   "they don't have mountains, they have icebergs.",
-   "Angelina Jolie went jogging there and almost lost an eye.",
-   "they don't have central heating, they just open a fridge in every room.",
-   "they believe global warming would be a good start.",
-   "Ron Jeremy once visited and was described as 'petite'.",
-   "Fargo was filmed in summertime.",
-   "a yeti won't go there.",
-   "they laughed at Superman's icecastle.",
-   "when you flush the toilet, it sounds like an icecube maker.",
-   "people come for the hospitality, but stay because they're frozen.",
-   "Thomas Lipton got the idea for icetea while drinking tea in Canadia.",
-   "the Snickers ice bar is just called Snickers.",
-   "every coffee is an iced coffee.",
-   "Kelvin was invented to give Canadia temperatures above 0.",
-   "'The Day After Tomorrow' was filmed there on a hot summer afternoon.",
-   "a fire once froze over.",
-   "it's not that cold, you hoser!",
-   "they add 'eh' add the end of each sentence to stop their lips from freezing.",
-   "a duck does have an echo."
+  [{k: "text", c: "Chuck Norris once visited and described it as lukewarm."},
+   {k: "text", c: "Sean Bean will never visit it."},
+   {k: "text", c: "pinguins moved away."},
+   {k: "text", c: "every drink is on the rocks."},
+   {k: "text", c: "they call icehockey, hockey."},
+   {k: "text", c: "they don't call it skiing, they call it walking."},
+   {k: "text", c: "they call icefishing, fishing."},
+   {k: "text", c: "every bear in canada is an icebear."},
+   {k: "text", c: "they don't have mountains, they have icebergs."},
+   {k: "text", c: "Angelina Jolie went jogging there and almost lost an eye."},
+   {k: "text", c: "they don't have central heating, they just open a fridge in every room."},
+   {k: "text", c: "they believe global warming would be a good start."},
+   {k: "text", c: "Ron Jeremy once visited and was described as 'petite'."},
+   {k: "text", c: "Fargo was filmed in summertime."},
+   {k: "text", c: "a yeti won't go there."},
+   {k: "text", c: "they laughed at Superman's icecastle."},
+   {k: "text", c: "when you flush the toilet, it sounds like an icecube maker."},
+   {k: "text", c: "people come for the hospitality, but stay because they're frozen."},
+   {k: "text", c: "Thomas Lipton got the idea for icetea while drinking tea in Canadia."},
+   {k: "text", c: "the Snickers ice bar is just called Snickers."},
+   {k: "text", c: "every coffee is an iced coffee."},
+   {k: "text", c: "Kelvin was invented to give Canadia temperatures above 0."},
+   {k: "text", c: "'The Day After Tomorrow' was filmed there on a hot summer afternoon."},
+   {k: "text", c: "a fire once froze over."},
+   {k: "text", c: "it's not that cold, you hoser!"},
+   {k: "text", c: "they add 'eh' add the end of each sentence to stop their lips from freezing."},
+   {k: "text", c: "a duck does have an echo."},
+   {k: "video", c: "https://mtc.cdn.vine.co/r/videos/ECE477F7011143396093686628352_3facab04e7c.0.1.17958842360645696189.mp4?versionId=Zqk9zvRupx7fYRK3i8rxn7mzi0YTnR9c"},
+   {k: "video", c: "https://pile.pjaspers.com/CArAgYWW8AEmCQP.mp4"},
+   {k: "image", c: "http://static.boredpanda.com/blog/wp-content/uploads/2015/03/icy-hair-freezing-contest-takhini-hot-springs-16.jpg", s: "http://www.boredpanda.com/icy-hair-freezing-contest-takhini-hot-springs/"}
   ].sample
 end
 
@@ -54,8 +57,21 @@ __END__
 @@ index
 %h1= "What's the weather like in Canadia?"
 %h2= @title
-%p= @content
-%h2= "You should probably wear long johns."
+
+%div.illustration
+  - if @content[:k] == "text"
+    %p= @content[:c]
+  - elsif @content[:k] == "image"
+    %p
+      %img{src: @content[:c], style: "max-width:100%;height:auto;"}
+  - elsif @content[:k] == "video"
+    %video{controls: "controls"}
+      %source{src: @content[:c], type: "video/mp4"}
+
+  - if source = @content[:s]
+    %a.source{href: source}= "More info"
+
+%h2.longjohns= "You should probably wear long johns."
 
 :javascript
   var _gauges = _gauges || [];

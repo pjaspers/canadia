d4ec487237f86e8376d6f49a6a138826c83f4c39
pjaspers <piet@pjaspers.com>
2015-04-11 20:33:05 +0200

    Adding slugs.

--- canadia.rb
+++ canadia.rb
@@ -2,43 +2,66 @@
 require "sinatra"
 require "haml"
 
-def random_sentence
-  [{k: "text", c: "Chuck Norris once visited and described it as lukewarm."},
-   {k: "text", c: "Sean Bean will never visit it."},
-   {k: "text", c: "pinguins moved away."},
-   {k: "text", c: "every drink is on the rocks."},
-   {k: "text", c: "they call icehockey, hockey."},
-   {k: "text", c: "they don't call it skiing, they call it walking."},
-   {k: "text", c: "they call icefishing, fishing."},
-   {k: "text", c: "every bear in canada is an icebear."},
-   {k: "text", c: "they don't have mountains, they have icebergs."},
-   {k: "text", c: "Angelina Jolie went jogging there and almost lost an eye."},
-   {k: "text", c: "they don't have central heating, they just open a fridge in every room."},
-   {k: "text", c: "they believe global warming would be a good start."},
-   {k: "text", c: "Ron Jeremy once visited and was described as 'petite'."},
-   {k: "text", c: "Fargo was filmed in summertime."},
-   {k: "text", c: "a yeti won't go there."},
-   {k: "text", c: "they laughed at Superman's icecastle."},
-   {k: "text", c: "when you flush the toilet, it sounds like an icecube maker."},
-   {k: "text", c: "people come for the hospitality, but stay because they're frozen."},
-   {k: "text", c: "Thomas Lipton got the idea for icetea while drinking tea in Canadia."},
-   {k: "text", c: "the Snickers ice bar is just called Snickers."},
-   {k: "text", c: "every coffee is an iced coffee."},
-   {k: "text", c: "Kelvin was invented to give Canadia temperatures above 0."},
-   {k: "text", c: "'The Day After Tomorrow' was filmed there on a hot summer afternoon."},
-   {k: "text", c: "a fire once froze over."},
-   {k: "text", c: "it's not that cold, you hoser!"},
-   {k: "text", c: "they add 'eh' add the end of each sentence to stop their lips from freezing."},
-   {k: "text", c: "a duck does have an echo."},
-   {k: "video", c: "https://mtc.cdn.vine.co/r/videos/ECE477F7011143396093686628352_3facab04e7c.0.1.17958842360645696189.mp4?versionId=Zqk9zvRupx7fYRK3i8rxn7mzi0YTnR9c"},
-   {k: "video", c: "https://pile.pjaspers.com/CArAgYWW8AEmCQP.mp4"},
-   {k: "image", c: "http://static.boredpanda.com/blog/wp-content/uploads/2015/03/icy-hair-freezing-contest-takhini-hot-springs-16.jpg", s: "http://www.boredpanda.com/icy-hair-freezing-contest-takhini-hot-springs/"}
-  ].sample
+# Remaining slugs:
+# mclachlan
+# morissette
+# north
+# page
+# polley
+# reeves
+# short
+# tilly
+# twain
+# tweed
+# warren
+# young
+def all_content
+  [{id: "levy", k: "text", c: "Chuck Norris once visited and described it as lukewarm."},
+   {id: "anderson", k: "text", c: "Sean Bean will never visit it."},
+   {id: "dallaire", k: "text", c: "pinguins moved away."},
+   {id: "twain", k: "text", c: "every drink is on the rocks."},
+   {id: "furtado", k: "text", c: "they call icehockey, hockey."},
+   {id: "warren", k: "text", c: "they don't call it skiing, they call it walking."},
+   {id: "tweed", k: "text", c: "they call icefishing, fishing."},
+   {id: "reeves", k: "text", c: "every bear in canada is an icebear."},
+   {id: "adams", k: "text", c: "they don't have mountains, they have icebergs."},
+   {id: "anderson", k: "text", c: "Angelina Jolie went jogging there and almost lost an eye."},
+   {id: "anka", k: "text", c: "they don't have central heating, they just open a fridge in every room."},
+   {id: "arnett", k: "text", c: "they believe global warming would be a good start."},
+   {id: "aykroyd", k: "text", c: "Ron Jeremy once visited and was described as 'petite'."},
+   {id: "baruchel", k: "text", c: "Fargo was filmed in summertime."},
+   {id: "beach", k: "text", c: "a yeti won't go there."},
+   {id: "candy", k: "text", c: "they laughed at Superman's icecastle."},
+   {id: "carrey", k: "text", c: "when you flush the toilet, it sounds like an icecube maker."},
+   {id: "cattrall", k: "text", c: "people come for the hospitality, but stay because they're frozen."},
+   {id: "cera", k: "text", c: "Thomas Lipton got the idea for icetea while drinking tea in Canadia."},
+   {id: "chong", k: "text", c: "the Snickers ice bar is just called Snickers."},
+   {id: "christensen", k: "text", c: "every coffee is an iced coffee."},
+   {id: "coates", k: "text", c: "Kelvin was invented to give Canadia temperatures above 0."},
+   {id: "cohen", k: "text", c: "'The Day After Tomorrow' was filmed there on a hot summer afternoon."},
+   {id: "cuthbert", k: "text", c: "a fire once froze over."},
+   {id: "dallaire", k: "text", c: "it's not that cold, you hoser!"},
+   {id: "dion", k: "text", c: "they add 'eh' add the end of each sentence to stop their lips from freezing."},
+   {id: "dobrev", k: "text", c: "a duck does have an echo."},
+   {id: "furtado", k: "video", c: "https://mtc.cdn.vine.co/r/videos/ECE477F7011143396093686628352_3facab04e7c.0.1.17958842360645696189.mp4?versionId=Zqk9zvRupx7fYRK3i8rxn7mzi0YTnR9c"},
+   {id: "henstridge", k: "video", c: "https://pile.pjaspers.com/CArAgYWW8AEmCQP.mp4"},
+   {id: "lavigne", k: "image", c: "http://static.boredpanda.com/blog/wp-content/uploads/2015/03/icy-hair-freezing-contest-takhini-hot-springs-16.jpg", s: "http://www.boredpanda.com/icy-hair-freezing-contest-takhini-hot-springs/"}
+  ].freeze
 end
 
 get "/" do
   @title = "It's so cold,"
-  @content = random_sentence
+  @content = all_content.sample
+  haml :index
+end
+
+get "/:slug" do |slug|
+  @title = "It's so cold,"
+  if content = all_content.detect {|d| d[:id] == slug.downcase}
+    @content = content
+  else
+    @content = "Not sure what you mean. You hoser! Let's assume cold."
+  end
   haml :index
 end
 
@@ -68,10 +91,14 @@ __END__
     %video{controls: "controls"}
       %source{src: @content[:c], type: "video/mp4"}
 
-  - if source = @content[:s]
-    %a.source{href: source}= "More info"
+  %div.metadata
+    %a.slug{href: @content[:id]}= "#"
+    - if source = @content[:s]
+      %a.source{href: source}= "More info"
 
-%h2.longjohns= "You should probably wear long johns."
+%h2.longjohns="You should probably wear long johns."
+%h2.seriously
+  %a{href: "/"}= "No seriously, how cold is it?"
 
 :javascript
   var _gauges = _gauges || [];

28796be381bf2b7b07848a4f89471efa31d7e3ec
pjaspers <pjaspers@air.local>
2020-12-22 23:49:26 +0100

    Test all the slugs

--- test/app_test.rb
+++ test/app_test.rb
@@ -15,12 +15,24 @@ class MyAppTest < Minitest::Test
 
   def test_it_declares_its_cold_in_canada
     get "/"
-    follow_redirect!
     assert_includes last_response.body, "It's so cold,"
   end
 
   def test_it_handles_slugs_it_doesn_t_know
     get "/nilo"
     assert_includes last_response.body, "You hoser!"
+  end
+
+  def test_no_double_slugs
+    all = YAML.load_file(File.expand_path("../../mounties.yml", __FILE__))
+    assert_equal all.count, all.collect{|a| a["slug"]}.uniq.count
+  end
+
+  YAML.load_file(File.expand_path("../../mounties.yml", __FILE__)).each do |data|
+    slug = data["slug"]
+    define_method("test_it_resolves_#{slug}") do
+      get "/#{slug}"
+      assert_includes last_response.body, data["content"]
+    end
   end
 end
